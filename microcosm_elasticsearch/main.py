7a735bebf195f766a0db97b3fba6793a69a5731a
Jesse Myers <jesse.myers@globality.com>
2017-06-12 21:11:39 -0700

    Add a query entry point

--- microcosm_elasticsearch/main.py
+++ microcosm_elasticsearch/main.py
@@ -3,6 +3,8 @@ CLI entry point hook.
 
 """
 from argparse import ArgumentParser
+from json import dump, loads
+from sys import stdout
 
 
 def createall_main(graph):
@@ -21,3 +23,21 @@ def createall_main(graph):
         only=args.only,
         skip=args.skip,
     )
+
+
+def query_main(graph, default_index):
+    parser = ArgumentParser()
+    parser.add_argument("--index", default=default_index)
+    parser.add_argument("--query", default='{"match_all": {}}')
+    args = parser.parse_args()
+
+    try:
+        query = loads(args.query)
+    except:
+        parser.error("query must be valid json")
+
+    response = graph.elasticsearch_client.search(
+        index=args.index,
+        body=dict(query=query),
+    )
+    dump(response, stdout)
