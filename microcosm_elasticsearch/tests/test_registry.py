d44b9dfdef3a46287d36f1cb42679acdf183d1ab
Sai Raveendran <sai.raveendran@globality.com>
2017-08-21 11:08:21 -0700

    Remove incorrect register test

--- microcosm_elasticsearch/tests/test_registry.py
+++ microcosm_elasticsearch/tests/test_registry.py
@@ -2,11 +2,10 @@
 Test registry management.
 
 """
-from hamcrest import assert_that, equal_to, is_, calling
+from hamcrest import assert_that, equal_to, is_
 from microcosm.api import create_object_graph
 
 from microcosm_elasticsearch.registry import IndexRegistry
-from microcosm_elasticsearch.assertions import assert_that_eventually
 
 
 def test_name_for():
@@ -27,17 +26,6 @@ def test_name_for_testing():
     assert_that(IndexRegistry.name_for(graph, name="foo"), is_(equal_to("foo_test")))
     assert_that(IndexRegistry.name_for(graph, version="v1"), is_(equal_to("example_v1_test")))
     assert_that(IndexRegistry.name_for(graph, name="foo", version="v2"), is_(equal_to("foo_v2_test")))
-
-
-def test_register():
-    graph = create_object_graph("example", testing=True)
-    index = graph.elasticsearch_index_registry.register()
-    assert_that_eventually(
-        calling(index.exists),
-        is_(equal_to(True)),
-        tries=5,
-        sleep_seconds=0.5,
-    )
 
 
 def test_createall():
